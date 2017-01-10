917c406c5f5c975e175a5ee962b396fa9e06e5e6
hueidou <hueidou163@gmail.com>
2017-01-10 08:58:16 +0800

    Update components.js

--- knockout2178/components.js
+++ knockout2178/components.js
@@ -1,4 +1,4 @@
 ko.components.register('like-widget', {
     viewModel: { require: 'component-like-widget.js' },
-    template: { require: 'component-like-widget.html' }
+    template: { require: 'text!component-like-widget.html' }
 });
